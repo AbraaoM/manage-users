8c0d1377ab1b8f7e39ced714d4382fd2768336ef
Abraão Moreira <abraaol.moreira@outlook.com>
2022-10-26 22:18:28 -0300

    Adiciona método post controller

---
+++ backend/Controllers/UsuarioController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using backend.Domain.Handlers;
+using backend.Domain.Command.Requests;
+
+namespace backend.Controllers {
+  [ApiController]
+  [Route("Usuarios")]
+
+  public class UsuarioController : ControllerBase { 
+    [HttpPost]
+    [Route("CriaUsuario")]
+    public IActionResult CriaUsuario(
+      [FromServices] ICriaUsuarioHandler handler,
+      [FromBody] CriaUsuarioRequest command) {
+        var response =  handler.Handle(command);
+        return Ok(response);
+    }
+  }
+}
