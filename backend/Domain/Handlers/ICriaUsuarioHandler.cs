3275f05a43c7761f23f2d05a8b5daa181803f5b5
Abraão Moreira <abraaol.moreira@outlook.com>
2022-10-26 22:17:49 -0300

    Fix namespace name

--- backend/Domain/Handlers/ICriaUsuarioHandler.cs
+++ backend/Domain/Handlers/ICriaUsuarioHandler.cs
@@ -1,7 +1,7 @@
 using backend.Domain.Command.Responses;
 using backend.Domain.Command.Requests;
 
-namespace backend.Domain {
+namespace backend.Domain.Handlers {
   public interface ICriaUsuarioHandler {
     CriaUsuarioResponse Handle(CriaUsuarioRequest command);
   }
